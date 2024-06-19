4a22a54a189f003be3dbabe24e078bfa80738aeb
Matheus-Estrella <matheus.a.estrella@gmail.com>
2024-06-19 09:42:40 -0300

    Path finalizado

--- FlightsApi/Controllers/FlightsController.cs
+++ FlightsApi/Controllers/FlightsController.cs
@@ -108,9 +108,9 @@ namespace FlightsApi.Controllers
             return CreatedAtAction("GetOne", new { flightNumber = flight.FlightNumber }, flight);
         }
 
-        // PATCH: Flights/ChangeStatus/5
-        [HttpPatch("Activate/{id}/{status}")]
-        public async Task<ActionResult> PatchStatus(int? id, bool status)
+        // PATCH: /ChangeStatus/1
+        [HttpPatch("ChangeStatus/{id}")]
+        public async Task<ActionResult> PatchStatus(int? id)
         {
             if (_context.Flight == null)
             {
@@ -128,9 +128,117 @@ namespace FlightsApi.Controllers
                 return NotFound();
             }
 
-            flight.Status = status;
+            flight.Status = !flight.Status;
+
+            try
+            {
+                _context.Flight.Update(flight);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightExists(flight.FlightNumber))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
             return NoContent();
+        }
+
+        // PATCH: /CheckSeats/1/Sell/1
+        // Cancel is for decrementation and Sell is for sold seats
+        [HttpPatch("CheckSeats/{id}/{operation}/{newSeats}")]
+        public async Task<ActionResult> PatchSeats(int? id, string? operation, int? newSeats)
+        {
+            if (_context.Flight == null)
+            {
+                return Problem("Entity set 'FlightsApiContext.Flights' is null.");
+            }
+
+            if (id == null)
+            {
+                return NotFound("Unavaliable Id");
+            }
+
+            var flight = await _context.Flight.Where(p => p.FlightNumber == id).Include(a => a.Plane).Include(c => c.Plane.Company).Include(c => c.Plane.Company.Address).Include(b => b.Arrival).FirstOrDefaultAsync();
+
+
+            /*
+             
+                var blog1 = context.Blogs
+                       .Where(b => b.Name == "ADO.NET Blog")
+                       .Include(b => b.Posts)
+                       .FirstOrDefault();
+            */
+
+            if (flight == null)
+            {
+                return NotFound("The flight doesn't exists");
+            }
+
+            // Checking the avaliable seats
+            int _num;
+            if(operation == "Cancel")
+            {
+                _num = -(newSeats ?? 0);
+            }else if(operation == "Sell")
+            {
+                _num = (newSeats ?? 0);
+            }
+            else
+            {
+                return NotFound("Invalid Operation, try these options:\n\tCancel\n\tSell");
+            }
+            bool? _notBlocked = flight.Plane.Company.Status;
+            if (_notBlocked == false)
+            {
+                return NotFound($"Blockd Company Transition, try to contact {flight.Plane.Company.Name}");
+            }
+            //if(flight.Status == false)
+            //{
+            //    return BadRequest("All seats are already sold up");
+            //}
+            int _maxCapacity = flight.Plane.Capacity;
+            int _currentCapacity = flight.Sales;
+            int _newCapacity = _currentCapacity + _num;
+
+            if (_newCapacity <= _maxCapacity)
+            {
+                flight.Sales = _newCapacity;
+                if(_newCapacity == _maxCapacity)
+                {
+                    flight.Status = !flight.Status;
+                }
+            }
+            else
+            {
+                return BadRequest($"There isn't avaliable flights to the selected destiny");
+            }
+
+            // Updating changes
+            try
+            {
+                _context.Flight.Update(flight);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightExists(flight.FlightNumber))
+                {
+                    return NotFound("The flight doesn't existis");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok($"{operation} on id {id} concluded");
         }
 
         private bool FlightExists(int id)
