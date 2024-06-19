39f0b9c59b615b4b066978cdc4f74c7b98684849
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-06-19 12:10:34 -0300

    fix (Models) Json Properties altered to match bank atributes.

--- Models/DTO/AircraftDTO.cs
+++ Models/DTO/AircraftDTO.cs
@@ -1,16 +1,15 @@
 ﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 
-namespace Models
+namespace Models.DTO
 {
-    public class Aircraft
+    public class AircraftDTO
     {
         [Key]
         [JsonProperty("rab")] public string Rab { get; set; }
         [JsonProperty("capacity")] public int Capacity { get; set; }
         [JsonProperty("dtRegistry")] public DateTime RegistryDate { get; set; }
         [JsonProperty("dtLastFlight")] public DateTime LastFlightDate { get; set; }
-        [JsonProperty("cnpjCompany")] public Company Company { get; set; }
+        [JsonProperty("cnpjCompany")] public string Company { get; set; }
     }
-
 }
